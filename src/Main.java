818f5b1fd353afc8a9241a2cf08bea715cb4fc77
azambembem <azamjan980713@gmail.com>
2024-11-24 17:31:15 +0900

    task-1

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+ //  2) intellij idea yordamida project yarating va quyidagilarni bajaring
+ //	dastur ishga tushganida salom sodda calculator degan
+ //	xabar ekranga chiqarilsin va uni pastidan quyidagicha hisobkitob chiqsin ekranga
+ //	3366 + 4522 = 7888
+ //	4522 - 3366 = 1156
+ //	4522 * 3366 = 15221052
+ //	4522 / 3366 = 1
+ //	eslatma hisobkitoblarni bajarishda javani o'zidan foydalaning
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Salom sodda calculator");
+
+        System.out.println(3366+ "+" + 4522+ "=" + (3366 + 4522));
+        System.out.println(4522+ "-" + 3366+ "=" + (4522 - 3366));
+        System.out.println(4522+ "*" + 3366+ "=" + (4522 * 3366));
+        System.out.println(4522+ "/" + 3366+ "=" + (4522 / 3366));
+    }
+}
