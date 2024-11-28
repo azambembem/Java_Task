75b24b09acf9f19b85b87b96a1535d0650306e22
azambembem <azamjan980713@gmail.com>
2024-11-28 15:40:02 +0900

    max and min number 찾기

--- src/Main.java
+++ src/Main.java
@@ -8,14 +8,27 @@ public class Main {
         int num1 = scanner.nextInt();
         System.out.println("2-sonni kiriting:");
         int num2 = scanner.nextInt();
-        int farq;
+        System.out.println("3-sonni kiriting:");
+        int num3 = scanner.nextInt();
+        System.out.println("4-sonni kiriting:");
+        int num4 = scanner.nextInt();
+        System.out.println("5-sonni kiriting:");
+        int num5 = scanner.nextInt();
+        int maxVal = num1;
 
-        if (num1 > num2) {
-            farq = num1 - num2;
-        } else {
-            farq = num2 - num1;
-        }
-        System.out.println("kiritilgan sonnlarni farqi" +farq);
+        if (num2 > maxVal) maxVal = num2;
+        if (num3 > maxVal) maxVal = num3;
+        if (num4 > maxVal) maxVal = num4;
+        if (num5 > maxVal) maxVal = num5;
+
+        int minVal = num1;
+        if( num2 < minVal) minVal = num2;
+        if (num3 < minVal) minVal = num3;
+        if (num4 < minVal) minVal = num4;
+        if (num5 < minVal) minVal = num5;
+
+        System.out.println("kiritilgan sonnlarni max xi" +maxVal);
+        System.out.println("kiritilgan sonlarni min ni" +minVal);
         scanner.close();
         }
     };
