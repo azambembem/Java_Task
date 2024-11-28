a5eac315565afdd5d80b9bffbbe8c59eab1e40a5
azambembem <azamjan980713@gmail.com>
2024-11-28 14:50:58 +0900

    if else month commit

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,39 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcolme to java 3");
-    }
+        Scanner scanner = new Scanner(System.in);
 
-}
+        System.out.println("1~12gacha bulgan sonni kiriting:");
+        int month = scanner.nextInt();
 
-
-
-
-
+        if (month == 1) {
+            System.out.println("YAN");
+        } else if (month == 2) {
+            System.out.println("FEB");
+        } else if (month == 3) {
+            System.out.println("MART");
+        } else if (month == 4) {
+            System.out.println("APREL");
+        } else if (month == 5) {
+            System.out.println("MAY");
+        } else if (month == 6) {
+            System.out.println("IYUN");
+        } else if (month == 7) {
+            System.out.println("IYUL");
+        } else if (month == 8) {
+            System.out.println("AVG");
+        } else if (month == 9) {
+            System.out.println("SEP");
+        } else if (month == 10) {
+            System.out.println("OCT");
+        } else if (month == 11) {
+            System.out.println("NOV");
+        } else if (month == 12) {
+            System.out.println("DEC");
+        } else {
+            System.out.println("notogri raqam kiritikgan: ");
+        }
+        scanner.close();
+        }
+    };
