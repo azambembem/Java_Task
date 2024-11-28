0eb419637c0081eaabee523b0e3f626ab11619da
azambembem <azamjan980713@gmail.com>
2024-11-28 15:24:16 +0900

    difference absolute num commit

--- src/Main.java
+++ src/Main.java
@@ -4,48 +4,18 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("1~12gacha bulgan sonni kiriting:");
-        int month = scanner.nextInt();
+        System.out.println("1-sonni kiriting:");
+        int num1 = scanner.nextInt();
+        System.out.println("2-sonni kiriting:");
+        int num2 = scanner.nextInt();
+        int farq;
 
-        switch (month){
-            case 1:
-                System.out.println("YAN");
-                break;
-            case 2 :
-                System.out.println("FEB");
-                break;
-            case 3 :
-                System.out.println("MAR");
-                break;
-            case 4:
-                System.out.println("APR");
-                break;
-            case 5:
-                System.out.println("MAY");
-                break;
-            case 6:
-                System.out.println("IYUN");
-                break;
-            case 7:
-                System.out.println("IYUL");
-                break;
-            case 8:
-                System.out.println("AVG");
-                break;
-            case 9:
-                System.out.println("SEP");
-                break;
-            case 10:
-                System.out.println("OCT");
-                break;
-            case 11:
-                System.out.println("NOV");
-                break;
-            case 12:
-                System.out.println("DEC");
-                break;
-            default: System.out.println("XATO");
-        };
+        if (num1 > num2) {
+            farq = num1 - num2;
+        } else {
+            farq = num2 - num1;
+        }
+        System.out.println("kiritilgan sonnlarni farqi" +farq);
         scanner.close();
         }
     };
