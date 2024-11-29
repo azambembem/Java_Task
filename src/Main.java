cb0cb892187f99567896f406b72223e6ec8865b6
azambembem <azamjan980713@gmail.com>
2024-11-29 23:48:36 +0900

    task commit

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,46 @@
-//1) ekrandan 3 ta double son kiritb oling va ularni yig'indisini
-//ayirmasini, ko'paytmasini va bo'linmasini shuningdek
-//	1-songa 2-sonni qo'shilganining 3-songa bo'lgandagi qoldig'ini
-//ekranga tushunarlik qilib chiqaring yani maksimal diyan qulay va tushunarlik
-//bo'lsin
-//
+//2) ekrandan bitta int soni kiritilsa ko'paytirishdan boshqasini
+//ishlatmasdan 1ta amal bilan sonning 2-darajasini toping
+//	2 ta amal bilan sonning 4-darajasini toping
+//	3 ta amal bilan sonning 6-darajasini toping
+//	4 ta amal bilan sonning 12-darajasini toping
+//	5 ta amal bilan sonning 16-darajasini toping va ekranga chiqaring
 
+
+//2
 import java.util.Scanner;
-public  class Main {
-    public static void main (String [] args) {
-       Scanner scanner = new Scanner(System.in);
-       System.out.print("1-sonni kiriting");
-       double a = scanner.nextDouble();
-       System.out.print("2-sonni kiriting");
-       double b = scanner.nextDouble();
-       System.out.print("3-sonni kiriting");
-       double c = scanner.nextDouble();
+public class Main {
+   public static void main(String[] args) {
 
-       double sum = a + b + c;
-       double ayirmasi = a - b - c;
-       double kopaytmasi = a * b * c;
-       double bolinmasi = a / b / c;
-       double remainder = (a + b) / c;
+   Scanner scanner = new Scanner(System.in);
+
+  System.out.println("xoxlagan sonni kiriting:");
+  int num = scanner.nextInt();
+
+  int ikkinchi_daraja = num * num; // ikkinchi daraja kelib chiqadi.
+
+  int tortinchi_dataja = ikkinchi_daraja * ikkinchi_daraja; // ikkinchi_darajani ikkisira uzini uziga ko'paytirilsa 4-chi darajasi kelib choqadi.
+
+  int oltinchi_daraja = ikkinchi_daraja * ikkinchi_daraja * ikkinchi_daraja;
+
+  int unikkinchi_daraja = oltinchi_daraja * oltinchi_daraja;
+
+  int unoltimchi_dataja = unikkinchi_daraja * ikkinchi_daraja * ikkinchi_daraja;
 
 
-       System.out.println("yigindisi" +sum);
-       System.out.println("ayirmasi" +ayirmasi);
-       System.out.println("kopaytmasi" +kopaytmasi);
-       System.out.println("bolinmasi" +bolinmasi);
-       System.out.println("1-son 2-sonni 3-songa bolganda qoldiqi" +remainder);
-       scanner.close();
-    };
-};
+  System.out.print("2-daraja" +ikkinchi_daraja);
+  System.out.println("4-daraja" +tortinchi_dataja);
+  System.out.println("6-daraja" +oltinchi_daraja);
+  System.out.println("12-daraja" +unikkinchi_daraja);
+  System.out.println(("16-daraja" +unoltimchi_dataja));
 
+
+
+      System.out.println("1-chi raqamni kiriting: ? ");
+      int a = scanner.nextInt();
+
+      String b = (a % 5 == 0 && a % 3 == 0 && a > 0) ? "5 va 3ga karrali musbat" : "aks holda 5 va 3 ga karralik emas";
+      System.out.println("natija " +b);
+
+   }
+}
+//
