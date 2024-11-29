0233ae63059a6ebde6c6901e8a0e8220468b3198
azambembem <azamjan980713@gmail.com>
2024-11-29 23:40:12 +0900

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,34 @@
+//1) ekrandan 3 ta double son kiritb oling va ularni yig'indisini
+//ayirmasini, ko'paytmasini va bo'linmasini shuningdek
+//	1-songa 2-sonni qo'shilganining 3-songa bo'lgandagi qoldig'ini
+//ekranga tushunarlik qilib chiqaring yani maksimal diyan qulay va tushunarlik
+//bo'lsin
+//
+
 import java.util.Scanner;
+public  class Main {
+    public static void main (String [] args) {
+       Scanner scanner = new Scanner(System.in);
+       System.out.print("1-sonni kiriting");
+       double a = scanner.nextDouble();
+       System.out.print("2-sonni kiriting");
+       double b = scanner.nextDouble();
+       System.out.print("3-sonni kiriting");
+       double c = scanner.nextDouble();
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+       double sum = a + b + c;
+       double ayirmasi = a - b - c;
+       double kopaytmasi = a * b * c;
+       double bolinmasi = a / b / c;
+       double remainder = (a + b) / c;
 
-        System.out.println("1-sonni kiriting:");
-        int num1 = scanner.nextInt();
-        System.out.println("2-sonni kiriting:");
-        int num2 = scanner.nextInt();
-        System.out.println("3-sonni kiriting:");
-        int num3 = scanner.nextInt();
-        System.out.println("4-sonni kiriting:");
-        int num4 = scanner.nextInt();
-        System.out.println("5-sonni kiriting:");
-        int num5 = scanner.nextInt();
-        int maxVal = num1;
 
-        if (num2 > maxVal) maxVal = num2;
-        if (num3 > maxVal) maxVal = num3;
-        if (num4 > maxVal) maxVal = num4;
-        if (num5 > maxVal) maxVal = num5;
-
-        int minVal = num1;
-        if( num2 < minVal) minVal = num2;
-        if (num3 < minVal) minVal = num3;
-        if (num4 < minVal) minVal = num4;
-        if (num5 < minVal) minVal = num5;
-
-        System.out.println("kiritilgan sonnlarni max xi" +maxVal);
-        System.out.println("kiritilgan sonlarni min ni" +minVal);
-        scanner.close();
-        }
+       System.out.println("yigindisi" +sum);
+       System.out.println("ayirmasi" +ayirmasi);
+       System.out.println("kopaytmasi" +kopaytmasi);
+       System.out.println("bolinmasi" +bolinmasi);
+       System.out.println("1-son 2-sonni 3-songa bolganda qoldiqi" +remainder);
+       scanner.close();
     };
+};
+
